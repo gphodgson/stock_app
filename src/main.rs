cf5fe4cc97fcde417ea99adc5a2c8aec274b0747
gphodgson <geoffphodgson@gmail.com>
2024-07-20 13:48:20 -0500

    init commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+slint::include_modules!();
+
+fn main() -> Result<(), slint::PlatformError> {
+    let ui = AppWindow::new()?;
+
+    ui.set_counter(12);
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.run()
+}
